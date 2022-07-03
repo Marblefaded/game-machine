4c6a47dfd9253e789aee60e366ecd8a2a6e0e562
Marblefade <sitnikov.vladimiir@mail.ru>
2022-07-03 23:05:40 +0700

    First stage: add in game with dice bets but with bugs :)

--- gamemachine/Form1.Designer.cs
+++ gamemachine/Form1.Designer.cs
@@ -32,6 +32,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -71,17 +76,67 @@
             this.label3.Size = new System.Drawing.Size(0, 41);
             this.label3.TabIndex = 3;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 277);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "<7";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(30, 327);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "7";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(30, 384);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 6;
+            this.button4.Text = ">7";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(176, 331);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(38, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 46);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(829, 452);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "t";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,5 +148,11 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private EventHandler textBox1_TextChanged;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox1;
+        private Label label4;
     }
 }
--- gamemachine/Form1.cs
+++ gamemachine/Form1.cs
@@ -2,6 +2,10 @@ namespace gamemachine
 {
     public partial class Form1 : Form
     {
+        int dice1 = 0;
+        int dice2 = 0;
+        int sum = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -11,14 +15,43 @@ namespace gamemachine
         {
             Random rnd = new Random();
 
-            int dice1 = rnd.Next(1,7);
+            dice1 = rnd.Next(1,7);
             label1.Text = "" + dice1;
 
-            int dice2 = rnd.Next(1, 7);
+            dice2 = rnd.Next(1, 7);
             label2.Text = "" + dice2;
 
-            int sum = dice1 + dice2;
+            sum = dice1 + dice2;
             label3.Text = "" + sum;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string bet = textBox1.Text;
+            int fbet = Convert.ToInt32(bet);
+
+            if (sum < 7)
+            {
+                int lseven = 2 * fbet;
+                label4.Text = "" + lseven;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string bet = textBox1.Text;
+            int fbet = Convert.ToInt32(bet);
+
+            int eseven = 5 * fbet;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string bet = textBox1.Text;
+            int fbet = Convert.ToInt32(bet);
+
+            int mseven = 2 * fbet;
         }
     }
 }
